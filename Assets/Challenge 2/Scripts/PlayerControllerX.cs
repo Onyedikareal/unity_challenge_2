0d80e83cd53d362edd5922a8d246d28003adafa3
Onyedikareal <frankonyeka25@gmail.com>
2022-09-05 20:32:49 +0100

    challenge 2 completed

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeInterval = 1.0f;
+    private float previousEventTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+            if (previousEventTime == 0.0 | Time.time - previousEventTime > timeInterval)
+            {
+                previousEventTime = Time.time;
+
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+            }
+
+        
+        
+    }
+}
